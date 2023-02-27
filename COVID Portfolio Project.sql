b86c0ec93023822022b3e72e7404625a1b026b09
leslienorma <126028793+leslienorma@users.noreply.github.com>
2023-02-27 12:32:02 -0800

    Update COVID Portfolio Project.sql

--- COVID Portfolio Project.sql
+++ COVID Portfolio Project.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 SELECT *
 FROM PortfolioProject.dbo.CovidDeaths
 WHERE continent IS NOT NULL
